9be9f07a8f759c5523181e422306010d455e2552
unknown <vietn0021@gmail.com>
2024-10-20 15:11:38 +0700

    demo

---
+++ main.js
@@ -0,0 +1,11 @@
+// Function to show the paper
+function showPaper() {
+    document.getElementById('popupPaper').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+// Function to close the paper
+function closePaper() {
+    document.getElementById('popupPaper').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
